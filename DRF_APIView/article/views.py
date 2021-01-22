d3290e80913bed6dddc658c41dcff325f93b5fd3
wiky-avis <peta-holding@yandex.ru>
2021-01-22 21:37:06 +0300

    использование простого класса APIView

---
+++ DRF_APIView/article/views.py
@@ -0,0 +1,37 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        # Параметр many сообщает сериализатору, что он будет сериализовать более одной статьи.
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+        # Создайте статью из приведенных выше данных
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({
+            "success": "Article '{}' updated successfully".format(article_saved.title)
+        })
+
+    def delete(self, request, pk):
+        # Получить объект с этим pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({"message": "Article with id `{}` has been deleted.".format(pk)}, status=204)
