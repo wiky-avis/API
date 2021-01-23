aa57ed2f77a5181340aa422ee3d99d5d49f54e5a
wiky-avis <peta-holding@yandex.ru>
2021-01-23 14:53:25 +0300

    ViewSets 2

--- DRF_ViewSets/article/urls.py
+++ DRF_ViewSets/article/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+# from django.urls import path
 
-from .views import ArticleView
+# from .views import ArticleView
 
-# app_name поможет выполнить обратный поиск.
-app_name = "articles"
+# # app_name поможет выполнить обратный поиск.
+# app_name = "articles"
 
 
-urlpatterns = [
-    path('articles/', ArticleView.as_view({'get': 'list'})),
-    path('articles/<int:pk>', ArticleView.as_view({'get': 'retrieve'})),
-]
+# urlpatterns = [
+#     path('articles/', ArticleView.as_view({'get': 'list'})),
+#     path('articles/<int:pk>', ArticleView.as_view({'get': 'retrieve'})),
+# ]
+
+#класс DefaultRouter позволяет значительно уменьшить код в URL
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
+
+router = DefaultRouter()
+router.register(r'articles', ArticleViewSet, basename='user')
+urlpatterns = router.urls
--- DRF_ViewSets/article/views.py
+++ DRF_ViewSets/article/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
-
 from .models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleView(viewsets.ViewSet):
-    """
-    Простой ViewSet, предназначенный для отображения или поиска статей.
-    """
-    def list(self, request):
-        queryset = Article.objects.all()
-        serializer = ArticleSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Article.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = ArticleSerializer(user)
-        return Response(serializer.data)
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
