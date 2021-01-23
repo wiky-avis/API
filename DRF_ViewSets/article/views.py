9e19781ad8bd0998075ce1806705f4f92156cae8
wiky-avis <peta-holding@yandex.ru>
2021-01-23 15:01:32 +0300

    ViewSets 3

--- DRF_ViewSets/article/views.py
+++ DRF_ViewSets/article/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from .models import Article
+from .models import Article, Author
 from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+
+    def perform_create(self, serializer):
+        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
