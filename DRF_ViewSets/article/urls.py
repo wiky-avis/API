a5a3345c90362db973ce3a86ba54c305e5dfb228
wiky-avis <peta-holding@yandex.ru>
2021-01-23 14:37:55 +0300

    ViewSets

--- DRF_ViewSets/article/urls.py
+++ DRF_ViewSets/article/urls.py
@@ -7,7 +7,6 @@ app_name = "articles"
 
 
 urlpatterns = [
-    path('articles/', ArticleView.as_view()),
-    # put
-    path('articles/<int:pk>', ArticleView.as_view()),
+    path('articles/', ArticleView.as_view({'get': 'list'})),
+    path('articles/<int:pk>', ArticleView.as_view({'get': 'retrieve'})),
 ]
